9544899df580b761034ad3e30c4e977eaafe6e64
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2020-08-21 09:47:57 +0200

    Update cs_dynamic_children_helper.sql
    fixing #21

--- cs_dynamic_children_helper.sql
+++ cs_dynamic_children_helper.sql
@@ -33,13 +33,18 @@ CREATE OR REPLACE FUNCTION cs_refresh_dynchilds_functions()
   RETURNS character varying AS
 $BODY$
 DECLARE
-   dropSchema boolean;
+   dropBackupSchema boolean;
+   renameSchema boolean;
 BEGIN
-SELECT EXISTS(SELECT * FROM information_schema.schemata WHERE schema_name = 'csdc_backup') INTO dropSchema;
-IF dropSchema THEN
+SELECT EXISTS(SELECT * FROM information_schema.schemata WHERE schema_name = 'csdc_backup') INTO dropBackupSchema;
+SELECT EXISTS(SELECT * FROM information_schema.schemata WHERE schema_name = 'csdc') INTO renameSchema;
+IF dropBackupSchema THEN
 	drop schema csdc_backup cascade;
 END IF;
-ALTER SCHEMA csdc RENAME TO csdc_backup;
+IF renameSchema THEN
+    ALTER SCHEMA csdc RENAME TO csdc_backup;
+END IF;
+
 create schema csdc;
 perform execute('CREATE OR REPLACE FUNCTION csdc.'||name||' RETURNS VARCHAR AS $$ select'''||regexp_replace(replace(code,'''',''''''),'(.*?)<ds::param.*>(.*?)</ds::param>(.*?)',E'\\1''||$\\2||''\\3','g')||'''::varchar $$ LANGUAGE ''sql'';') from cs_dynamic_children_helper;
     RETURN 'Functions refreshed';
@@ -50,6 +55,3 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-
-
-
